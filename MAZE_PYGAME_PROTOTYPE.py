bf791d011a499d462728cedbd27a748c7a96a453
Casserole <casseroliravioli@gmail.com>
2022-04-13 17:08:51 +0100

    Created player character

--- MAZE_PYGAME_PROTOTYPE.py
+++ MAZE_PYGAME_PROTOTYPE.py
@@ -2,7 +2,7 @@ import pygame
 from random import choice, random
 pygame.init()
 
-# REMEMBER THE DIFFERENCE BETWEEN X/Y AND ROW/COLUMN
+# REMEMBER THE DIFFERENCE BETWEEN X/Y AND ROW/COLUMN.
 #  X 0 1 2 3 4   |  C 0 1 2 3 4
 # Y              | R
 # 0              | 0
@@ -11,14 +11,14 @@ pygame.init()
 # 3              | 3
 # 4              | 4
 
-# Declaring variables
-MAX_WIDTH = 1000
-MAX_HEIGHT = 800
+# Declaring variables.
+MAX_WIDTH = 800
+MAX_HEIGHT = 600
 
-# Creating the surface which will be displayed
+# Creating the surface which will be displayed.
 DisplaySurface = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
-DisplaySurface.fill(pygame.Color("white"))
-pygame.display.set_caption("test")
+DisplaySurface.fill((0, 0, 0))
+pygame.display.set_caption("Game")
 
 # Basic class that acts as a coordinate on a map.
 # Only needed for other classes to inherit from, will never be placed
@@ -64,16 +64,16 @@ class Passage(Tile):
 # Each space will hold either a wall, a cell, or a passage.
 class Grid():
     def __init__(self, vertical_cells, horizontal_cells, tile_pixels):
-        # Maximum vertical and horizontal tiles in the maze.
+        # Maximum vertical and horizontal tiles in the grid.
         self.MAX_VER_TILES = vertical_cells*2+1
         self.MAX_HOR_TILES = horizontal_cells*2+1
         # The lengths of the sides of a tile square in pixels.
         self.tile_pixels = tile_pixels
         # Total vertical and horizontal pixels used for the surface on which
-        # the maze will be drawn.
+        # the grid will be drawn.
         self.total_ver_pixels = self.MAX_VER_TILES*tile_pixels
         self.total_hor_pixels = self.MAX_HOR_TILES*tile_pixels
-        # Initialize a blank grid of walls and cells.
+        # Initialize a grid of walls and cells.
         self.grid = [[self.__GenerateCellorWall(row, col) for col in range(0, self.MAX_HOR_TILES)] for row in range(0, self.MAX_VER_TILES)]
 
     # Method used in the init of the grid.
@@ -83,8 +83,8 @@ class Grid():
         else:
             return Wall(row, col)
 
-    # Displays the maze in a textual form.
-    def PrintMaze(self):
+    # Displays the grid in a textual form.
+    def PrintGrid(self):
         for row in self.grid:
             print()
             for tile in row:
@@ -93,20 +93,20 @@ class Grid():
                 else:
                     print('  ', end='')
 
-    # A maze is drawn on a new surface which is then returned.
-    def DrawMaze(self):
-        maze_surf = pygame.Surface((self.total_hor_pixels, self.total_ver_pixels))
-        maze_surf.fill(pygame.Color("white"))
+    # The grid is drawn on a new surface which is then returned.
+    def DrawGrid(self):
+        grid_surf = pygame.Surface((self.total_hor_pixels, self.total_ver_pixels))
+        grid_surf.fill((0, 0, 0))
         count1 = 0
         count2 = 0
         for row in self.grid:
             for tile in row:
                 if tile.type == "WALL":
-                    pygame.draw.rect(maze_surf, pygame.Color("black"), pygame.Rect(count1, count2, self.tile_pixels, self.tile_pixels))
+                    pygame.draw.rect(grid_surf, (255, 255, 255), pygame.Rect(count1, count2, self.tile_pixels, self.tile_pixels))
                 count1 += self.tile_pixels
             count1 = 0
             count2 += self.tile_pixels
-        return maze_surf
+        return grid_surf
 
     # Return the tile on that row and column.
     def GetTile(self, row, col):
@@ -161,12 +161,42 @@ class Grid():
                 self.CarvePassage(cell, next_cell)
             self.RecursiveBacktracker(next_cell, loop_chance)
 
-# Created a maze object, invoked recursive backtracker, drew the maze, blitted it, and flipped the display.
+# Player class.
+class Player(pygame.sprite.Sprite):
+    def __init__(self, size):
+        super().__init__()
+        self.surf = pygame.Surface((size, size))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys, speed):
+            # Moves player based on speed.
+            if pressed_keys[pygame.K_UP]:
+                self.rect.move_ip(0, -speed)
+            if pressed_keys[pygame.K_DOWN]:
+                self.rect.move_ip(0, speed)
+            if pressed_keys[pygame.K_LEFT]:
+                self.rect.move_ip(-speed, 0)
+            if pressed_keys[pygame.K_RIGHT]:
+                self.rect.move_ip(speed, 0)
+
+            # Prevents out of bounds movement.
+            if self.rect.left < 0:
+                self.rect.left = 0
+            if self.rect.right > MAX_WIDTH:
+                self.rect.right = MAX_WIDTH
+            if self.rect.top < 0:
+                self.rect.top = 0
+            if self.rect.bottom > MAX_HEIGHT:
+                self.rect.bottom = MAX_HEIGHT
+
+# Initialize grid object, generate maze, and get surface object.
 maze1 = Grid(12, 12, 20)
-maze1.RecursiveBacktracker(maze1.GetTile(1, 1), 0.0)
-maze_surface = maze1.DrawMaze()
-DisplaySurface.blit(maze_surface, ((MAX_WIDTH-maze1.total_hor_pixels)//2, ((MAX_HEIGHT-maze1.total_ver_pixels)//2)))
-pygame.display.flip()
+maze1.RecursiveBacktracker(maze1.GetTile(1, 1), 0.05)
+maze1_surface = maze1.DrawGrid()
+
+# Create player.
+p1 = Player(10)
 
 # Created a clock object.
 clock = pygame.time.Clock()
@@ -182,7 +212,20 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Makes the game run at 60 FPS.
+    # Update character's position.
+    pressed_keys = pygame.key.get_pressed()
+    p1.update(pressed_keys, 3)
+
+    # Draw everything.
+    DisplaySurface.fill((0, 0, 0))
+    DisplaySurface.blit(maze1_surface, ((MAX_WIDTH-maze1.total_hor_pixels)//2, ((MAX_HEIGHT-maze1.total_ver_pixels)//2)))
+    DisplaySurface.blit(p1.surf, p1.rect)
+
+    # Flip display.
+    pygame.display.flip()
+
+    # Makes the game run at a set FPS.
     clock.tick(60)
 
+# Quits pygame to successfuly close the game.
 pygame.quit()
