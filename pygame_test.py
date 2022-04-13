b8ac1c3c9a8fca0e50b3c3a1d6d010066dcc9eaa
Casserole <casseroliravioli@gmail.com>
2022-04-13 14:21:21 +0100

    Used auto format on pygame_test.py

--- pygame_test.py
+++ pygame_test.py
@@ -1,38 +1,41 @@
-#https://coderslegacy.com/python/python-pygame-tutorial/
-#https://realpython.com/pygame-a-primer/
+# https://coderslegacy.com/python/python-pygame-tutorial/
+# https://realpython.com/pygame-a-primer/
 
-import pygame, random
+import pygame
+import random
 pygame.init()
 
-#Declaring variables
+# Declaring variables
 MAX_WIDTH = 1000
 MAX_HEIGHT = 800
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-#Creating enemy and cloud spawning events and making them appear every interval
+# Creating enemy and cloud spawning events and making them appear every interval
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 250)
 ADDCLOUD = pygame.USEREVENT + 2
 pygame.time.set_timer(ADDCLOUD, 1000)
 
-#Creating surface to draw on
+# Creating surface to draw on
 display_surface = pygame.display.set_mode((MAX_WIDTH, MAX_HEIGHT))
 display_surface.fill(WHITE)
 
-#Player class
+# Player class
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surf = pygame.image.load("misc/texture_player1.png").convert()
         self.surf.set_colorkey(WHITE, pygame.RLEACCEL)
         self.rect = self.surf.get_rect()
-    
-    #Checks pressed keys and moves accordingly
+
+    # Checks pressed keys and moves accordingly
     def update(self, pressed_keys):
         speed = 10
-        #Updates player's position by speed depending on the button press
+        # Updates player's position by speed depending on the button press
         if pressed_keys[pygame.K_UP]:
             self.rect.move_ip(0, -speed)
         if pressed_keys[pygame.K_DOWN]:
@@ -41,8 +44,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.move_ip(-speed, 0)
         if pressed_keys[pygame.K_RIGHT]:
             self.rect.move_ip(speed, 0)
-        
-        #Prevents out of bounds movement
+
+        # Prevents out of bounds movement
         if self.rect.left < 0:
             self.rect.left = 0
         if self.rect.right > MAX_WIDTH:
@@ -52,13 +55,16 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom > MAX_HEIGHT:
             self.rect.bottom = MAX_HEIGHT
 
-#Enemy class
+# Enemy class
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surf = pygame.image.load("misc/texture_enemy.png").convert()
         self.surf.set_colorkey((255, 255, 255), pygame.RLEACCEL)
-        self.rect = self.surf.get_rect(center=(random.randint(MAX_WIDTH + 20, MAX_WIDTH + 100), random.randint(0, MAX_HEIGHT)))
+        self.rect = self.surf.get_rect(center=(random.randint(
+            MAX_WIDTH + 20, MAX_WIDTH + 100), random.randint(0, MAX_HEIGHT)))
         self.speed = random.randint(5, 20)
 
     # Move the sprite based on speed
@@ -68,19 +74,22 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
+
 class Cloud(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.surf = pygame.image.load("misc/texture_cloud.png").convert()
         self.surf.set_colorkey(BLACK, pygame.RLEACCEL)
         # The starting position is randomly generated
-        self.rect = self.surf.get_rect(center=(random.randint(MAX_WIDTH + 20, MAX_WIDTH + 100), random.randint(0, MAX_HEIGHT)))
-    
+        self.rect = self.surf.get_rect(center=(random.randint(
+            MAX_WIDTH + 20, MAX_WIDTH + 100), random.randint(0, MAX_HEIGHT)))
+
     def update(self):
         self.rect.move_ip(-5, 0)
         if self.rect.right < 0:
             self.kill()
-            
+
+
 p1 = Player()
 enemies = pygame.sprite.Group()
 clouds = pygame.sprite.Group()
@@ -89,50 +98,50 @@ all_sprites.add(p1)
 
 clock = pygame.time.Clock()
 
-#Game loop
+# Game loop
 running = True
 while running:
-    #Event handler
+    # Event handler
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
         elif event.type == pygame.QUIT:
             running = False
-        #Custom event, adds an enemy to two groups
+        # Custom event, adds an enemy to two groups
         elif event.type == ADDENEMY:
             new_enemy = Enemy()
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
-        #Custom event, adds a cloud to two groups
+        # Custom event, adds a cloud to two groups
         elif event.type == ADDCLOUD:
             new_cloud = Cloud()
             clouds.add(new_cloud)
             all_sprites.add(new_cloud)
-    
-    #Updates the character's position
+
+    # Updates the character's position
     pressed_keys = pygame.key.get_pressed()
     p1.update(pressed_keys)
-    
-    #Checks for collisions between the character and any enemies
+
+    # Checks for collisions between the character and any enemies
     if pygame.sprite.spritecollideany(p1, enemies):
-        running = False       
-    
-    #Fills everything with a color
+        running = False
+
+    # Fills everything with a color
     display_surface.fill(pygame.Color("skyblue"))
-    
-    #Draws all sprites on the screen
+
+    # Draws all sprites on the screen
     for entity in all_sprites:
         display_surface.blit(entity.surf, entity.rect)
-    
-    #Updates the cloud and enemy positions
+
+    # Updates the cloud and enemy positions
     clouds.update()
     enemies.update()
-    
-    #Draws everything on the screen
+
+    # Draws everything on the screen
     pygame.display.flip()
-    
-    #Sets FPS
+
+    # Sets FPS
     clock.tick(60)
-  
+
 pygame.quit()
